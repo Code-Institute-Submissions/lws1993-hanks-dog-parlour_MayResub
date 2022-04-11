e236ac831aa3fde71559c7c44e803d1e6c9a0684
lws1993 <lukesmallman15@gmail.com>
2022-04-11 11:03:30 +0000

    Product admin - add product view, url and template

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -72,3 +74,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
