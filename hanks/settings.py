09de8e7fa6e120aa37251a7da330bdccf056c3a5
lws1993 <lukesmallman15@gmail.com>
2022-04-11 14:09:18 +0000

    Deployment part 1

--- hanks/settings.py
+++ hanks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -126,10 +127,11 @@ WSGI_APPLICATION = 'hanks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
