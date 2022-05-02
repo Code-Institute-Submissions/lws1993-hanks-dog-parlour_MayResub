d3bb1f42e723ac74a8d7a5a87c90055944d9abfc
lws1993 <lukesmallman15@gmail.com>
2022-05-02 18:41:34 +0000

    Added reviews to my homepage

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,12 +2,4 @@ from django.contrib import admin
 from .models import Review
 
 
-class ReviewAdmin(admin.ModelAdmin):
-    model = Review
-    readonly_fields = (
-        "reviewer",
-        "review",
-        "stars",
-        )
-
-admin.site.register(Review, ReviewAdmin)
+admin.site.register(Review)
